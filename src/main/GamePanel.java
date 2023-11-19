342c392036c7a8232fd2764564dd8bd50b2bba9e
Davide Petroni <petroni.davide@educ.di.unito.it>
2023-11-19 15:47:37 +0100

    Modifico gamepanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,13 +7,15 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
 public class GamePanel extends JPanel implements Runnable{
 
     //SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;    //tabella da 16X12
     final int screenWidth = tileSize * maxScreenCol;    //768 pixels
@@ -23,6 +25,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     //Set player's default position
     int playerX = 100;
@@ -96,18 +99,7 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update(){
-
-        if(keyH.upPressed == true)
-            playerY -= playerSpeed;           
-
-        else if(keyH.downPressed == true)
-            playerY += playerSpeed;
-
-        else if(keyH.leftPressed == true)
-            playerX -= playerSpeed;
-
-        else if(keyH.rightPressed == true)
-            playerX += playerSpeed;
+        player.update();
     }
 
     public void paintComponent(Graphics g){
@@ -115,8 +107,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
     }
 }
